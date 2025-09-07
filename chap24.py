6b4859c03702b0e698f1dcf5ffcc646105322490
VaishnaviLav <156794763+VaishnaviLav@users.noreply.github.com>
2025-09-07 20:17:42 +0530

    Create chap24.py

---
+++ chap24.py
@@ -0,0 +1,20 @@
+a=32
+t=type(a) #class <int>
+print(t)
+
+a=32.45
+t=type(a) 
+print(t)
+
+a="Harry"
+t=type(a) 
+print(t)
+
+a="23"
+t=type(a) 
+print(t)
+
+a="23.2"
+b=float(a) #conversion from string to float
+t=type(b) 
+print(t)
