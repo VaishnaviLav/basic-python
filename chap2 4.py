217b683ab816c74f95ddd871e06093bfbc599908
VaishnaviLav <156794763+VaishnaviLav@users.noreply.github.com>
2025-09-07 20:16:57 +0530

    Create chap2 4.py

---
+++ chap2 4.py
@@ -0,0 +1,21 @@
+# Arithmetic operators
+a = 34
+b = 44
+c = a + b
+print(c)
+
+#Assignment oprators
+a=4-2 #Assign 4-2 in a 
+b=6 
+b+=3 #Yncrement the value of b by 3 and then assign it to b
+print(b)
+b-=3 
+print(b)
+
+# Comparison operators
+d= 5> 4 
+print(d)
+
+# Logical operators  
+e=True or False # Truth table shit 
+print(e)
